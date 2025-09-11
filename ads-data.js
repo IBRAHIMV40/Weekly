a1ce9c876df7ca73fb3b3b4169fd39fcf461ac84
IBRAHIMV40 <Ibraheemlawal40@gmail.com>
2025-09-11 09:31:09 +0100

    Add files via upload

---
+++ ads-data.js
@@ -0,0 +1,20 @@
+const adsData = [
+    {
+        name: "Cheelee: Short Videos f...",
+        icon: "https://tinylink.info/119AL",
+        rating: "4.5",
+        link: "https://tinylink.info/119Aa"
+    },
+    {
+        name: "CoolApp",
+        icon: "https://tinylink.info/119A0",
+        rating: "4.8",
+        link: "https://tinylink.info/YchD"
+    },
+    {
+        name: "Z League: Mini Games & Friends",
+        icon: "https://tinylink.info/Ycg4",
+        rating: "4.0",
+        link: "https://tinylink.info/119yG"
+    }
+];
---
+++ profile-apps.js
@@ -0,0 +1,26 @@
+const profileApps = [
+    {
+        name: "Hausa Movies - Cinema & Films,",
+        description: "Watch the latest Hausa movies and films online",
+        link: "https://www.appcreator24.com/app3501058-ks4rfa",
+        icon: "https://bit.ly/434z6xR",
+    },
+    {
+        name: "PalmPay - Transfers, Bills,",
+        description: "Money transfers, pay for airtime and bills, earn rewards.",
+        link: "https://link.palmpay.com/828uc",
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_6zFCuqWj00YpgpbbMr-8Mx7hycUWdg-5McXnyYoWDA&s",
+    },
+    {
+        name: "OPay - Beyond Banking",
+        description: "Send money, pay bills, and more.",
+        link: "https://opay.ng/s/1jaSm",
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQva4hX_z36R5w15vmXrRgv4rQBEuz0LO0e0_UlrxZN7Zed4DpkIEXhFsQ&s=10",
+    },
+    {
+        name: "Moniepoint Personal Banking",
+        description: "Send money, request a debit card, and manage savings.",
+        link: "https://join.moniepoint.com?adj_t=15ha060e&rC=QWLR527",
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6magnMBrfTnuxUbS4lgRTQ_i6p5rIxxmwrDFBAhV7g&s",
+    },
+];
